ddc75df8600fc5d19d4b6a3d7a3155e7679f09d5
Yurii Sykal <108358132+yuriiSY@users.noreply.github.com>
2023-11-08 19:56:54 +0000

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,4 +14,32 @@ iframePlayer.on('timeupdate', function (time) {
 const serializedState = localStorage.getItem('videoplayer-current-time');
 serializedState === null ? undefined : JSON.parse(serializedState);
 
-iframePlayer.setCurrentTime(Nunmer(serializedState));
+iframePlayer.setCurrentTime(Number(serializedState));
+
+// import Player from '@vimeo/player';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// player.on('timeupdate', onPlay);
+
+// function onPlay(data) {
+//   const name = 'videoplayer-current-time';
+//   const time = data.seconds;
+//   saveTolS(name, time);
+// }
+// const newTime = Number(getFromLS('videoplayer-current-time'));
+// player.setCurrentTime(newTime);
+
+// function saveTolS(key, value) {
+//   localStorage.setItem(key, JSON.stringify(value));
+// }
+
+// function getFromLS(key) {
+//   const data = localStorage.getItem(key);
+//   try {
+//     return JSON.parse(data);
+//   } catch {
+//     return data;
+//   }
+// }
