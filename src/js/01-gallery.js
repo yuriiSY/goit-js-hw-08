794ff1068d16e20243ac04be9e4de317feebac2a
Yurii Sykal <108358132+yuriiSY@users.noreply.github.com>
2023-11-08 19:53:23 +0000

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </li>`
+    )
+    .join('');
+}
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+const player = document.querySelector('#vimeo-player');
+
+const iframePlayer = new Player(player);
+
+iframePlayer.on('timeupdate', function (time) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(time.seconds)
+  );
+});
+
+const serializedState = localStorage.getItem('videoplayer-current-time');
+serializedState === null ? undefined : JSON.parse(serializedState);
+
+iframePlayer.setCurrentTime(Nunmer(serializedState));
