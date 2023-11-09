2db08e0dd79b9aa22c70d8397934cc2c0ce70304
Yurii Sykal <108358132+yuriiSY@users.noreply.github.com>
2023-11-09 13:26:31 +0000

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,45 +1,27 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const player = document.querySelector('#vimeo-player');
 
 const iframePlayer = new Player(player);
 
-iframePlayer.on('timeupdate', function (time) {
-  localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(time.seconds)
-  );
-});
+iframePlayer.on(
+  'timeupdate',
+  throttle(function (time) {
+    console.log(time);
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(time.seconds)
+    );
+  }, 1000)
+);
 
-const serializedState = localStorage.getItem('videoplayer-current-time');
-serializedState === null ? undefined : JSON.parse(serializedState);
+let serializedState = localStorage.getItem('videoplayer-current-time');
+try {
+  serializedState =
+    serializedState === null ? undefined : JSON.parse(serializedState);
+} catch (err) {
+  console.log(err);
+}
 
-iframePlayer.setCurrentTime(Number(serializedState));
-
-// import Player from '@vimeo/player';
-
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-
-// player.on('timeupdate', onPlay);
-
-// function onPlay(data) {
-//   const name = 'videoplayer-current-time';
-//   const time = data.seconds;
-//   saveTolS(name, time);
-// }
-// const newTime = Number(getFromLS('videoplayer-current-time'));
-// player.setCurrentTime(newTime);
-
-// function saveTolS(key, value) {
-//   localStorage.setItem(key, JSON.stringify(value));
-// }
-
-// function getFromLS(key) {
-//   const data = localStorage.getItem(key);
-//   try {
-//     return JSON.parse(data);
-//   } catch {
-//     return data;
-//   }
-// }
+iframePlayer.setCurrentTime(Number(currentTime));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+let data = {};
+
+updateState();
+
+form.addEventListener(
+  'input',
+  throttle(function (evnt) {
+    evnt.preventDefault;
+    data[evnt.target.name] = evnt.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(data));
+  }, 500)
+);
+
+form.addEventListener('submit', function (evnt) {
+  evnt.preventDefault();
+  data = {
+    email: evnt.currentTarget.elements.email.value,
+    message: evnt.currentTarget.elements.message.value,
+  };
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(data);
+});
+
+function updateState() {
+  let state = localStorage.getItem('feedback-form-state');
+  if (state) {
+    state = checkParse(state);
+    form.elements.email.value = state.email;
+    form.elements.message.value = state.message;
+  }
+}
+
+function checkParse(state) {
+  try {
+    return JSON.parse(state);
+  } catch (err) {
+    console.log(err);
+  }
+}
